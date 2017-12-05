6826593e7636dbf5e0bfd5509897f075000c6f04
apjanco <ajanco@haverford.edu>
2017-12-05 15:11:30 -0500

    fixe dqb_db

--- dqb_db.py
+++ dqb_db.py
@@ -6,14 +6,21 @@ collection = db['dqb_collection']
 
 def get(key,term):
     # Convert from string to ObjectId:
-    document = client.db.collection.find_one({ key: term})
+    document = collection.find_one({ key : term})
+    print(document)
+    return document
 
 def count(key,term):
-	collection.find({key : term}).count()
-
+	count = collection.find({ key : term}).count()
+	print(count)
+	return count
 
 def search(key,term):
-	result = collection.find({key : term})
-	print(result.value())
+	result = collection.find({ key : term})
+	for stuff in result:
+		print(stuff)	
+	return result
 
+get('file','DQB_ECCLES-EDRIDGE_Page_080.jpg.jpg')
+print("Hello!")
 
