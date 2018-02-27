95a453d47aef84d7f3dce54372ab26156c660263
root <root@DQB-dev.localdomain>
2018-02-27 20:12:32 +0000

    minor changes to mongo.py

--- util/mongo.py
+++ util/mongo.py
@@ -5,10 +5,10 @@ db = client['dqb_database']
 collection = db['dqb_collection']
 
 def get(key,term):
-    # Convert from string to ObjectId:
-    document = collection.find_one({ key : term})
-    print(document)
-    return document
+	# Convert from string to ObjectId:
+	document = collection.find_one({ key : term})
+	# print(document)
+	return document
 
 def count(key,term):
 	count = collection.find({ key : term}).count()
@@ -17,10 +17,10 @@ def count(key,term):
 
 def search(key,term):
 	result = collection.find({ key : term})
-	print(result)	
+	# print(result)	
 	return result
 
-#result = get('file','DQB_ECCLES-EDRIDGE_Page_080.jpg.jpg')
+result = get('file','DQB_HARTLEY-HEDLEY-61.jpg')
 #print(result['data']['responses']['textAnnotations'])
 
 #full text
@@ -29,10 +29,10 @@ def search(key,term):
 
 #all word results
 for text in result['data']['responses'][0]['textAnnotations']:
-    print(text['description'])
+	print(text['description'])
 #exclude 0 index    
 for text in result['data']['responses'][0]['textAnnotations'][1:]:
-    print(text['description'])
+	print(text['description'])
 
 print("Hello!")
 
